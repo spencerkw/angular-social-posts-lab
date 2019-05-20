43d858a001667f1acc349f309414929408d61175
Spencer K-W <keashlywarters@gmail.com>
2019-05-20 14:57:39 -0400

    initial project setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SocialPostsComponent } from './social-posts/social-posts.component';
+import { PostComponent } from './post/post.component';
+import { PostFormComponent } from './post-form/post-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SocialPostsComponent,
+    PostComponent,
+    PostFormComponent
   ],
   imports: [
     BrowserModule
